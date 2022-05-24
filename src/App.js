28c203e80379f577abca04c18b1367b20dfeeb3a
root <joseph@phobia.dev>
2022-05-24 16:56:58 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,116 +1,134 @@
-import { useState, useEffect } from "react"
-import stepOneGeneration from "./gameOfLife"
-import configurations from "./configurations"
-import Board from "./components/Board"
-import playButton from "./images/playButton.png"
-import pauseButton from "./images/pauseButton.png"
+import { useState, useEffect } from "react";
+import stepOneGeneration from "./gameOfLife";
+import configurations from "./configurations";
+import Board from "./components/Board";
+import playButton from "./images/playButton.png";
+import pauseButton from "./images/pauseButton.png";
 
 function saveLayout(board) {
-  let solids = []
+  let solids = [];
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[0].length; j++) {
       if (board[i][j]) {
-        solids.push(`[${i},${j}]`)
+        solids.push(`[${i},${j}]`);
       }
     }
   }
-  navigator.clipboard.writeText(`[${solids}]`)
+  navigator.clipboard.writeText(`[${solids}]`);
 }
 
-function fillBoard(rows,cols, randomised) {
-  let array = []
-  let row
+function fillBoard(rows, cols, randomised) {
+  let array = [];
+  let row;
   for (let i = 0; i < rows; i++) {
-    row = []
+    row = [];
     for (let j = 0; j < cols; j++) {
-      row.push(randomised ? (Math.random() < 0.3 ? 1: 0) : 0)
+      row.push(randomised ? (Math.random() < 0.3 ? 1 : 0) : 0);
     }
-    array.push(row)
+    array.push(row);
   }
-  return array
+  return array;
 }
 
 function sleep(ms) {
-  return new Promise((resolve,ms) => setTimeout(resolve,ms))
+  return new Promise((resolve, ms) => setTimeout(resolve, ms));
 }
 
 export default function App() {
-  let interval
-  const [rows, setRows] = useState(60)
-  const [cols, setCols] = useState(60)
-  const [speed, setSpeed] = useState(100)
-  const [board, setBoard] = useState(fillBoard(rows,cols,true))
-  const [playing, setPlaying] = useState(false)
-  
+  let interval;
+  const [rows, setRows] = useState(60);
+  const [cols, setCols] = useState(60);
+  const [speed, setSpeed] = useState(100);
+  const [board, setBoard] = useState(fillBoard(rows, cols, true));
+  const [playing, setPlaying] = useState(false);
+  const [configuration, setConfiguration] = useState("glider gun")
+
   useEffect(() => {
-    window.addEventListener("copy",() => {
-      saveLayout(board)
-    })
-  }, [board])
+    window.addEventListener("copy", () => {
+      saveLayout(board);
+    });
 
+  }, [board]);
 
-  function handleBoxClick(row,col) {
-    let copyBoard = board.slice()
-    copyBoard[row][col] = board[row][col] ? 0 : 1
-    setBoard(copyBoard)
+  function handleBoxClick(row, col) {
+    let copyBoard = board.slice();
+    copyBoard[row][col] = board[row][col] ? 0 : 1;
+    setBoard(copyBoard);
   }
 
-
   function setLayout(layout) {
-    let newBoard = fillBoard(rows,cols,false)
+    let newBoard = fillBoard(rows, cols, false);
 
     for (let item of layout) {
       if (item[0] >= rows || item[1] >= cols) {
-        alert("this configuration is too large for the current board")
-        return
+        alert("this configuration is too large for the current board");
+        return;
       }
-      newBoard[item[0]][item[1]] = 1
-      }
-      setBoard(newBoard)
-      }
+      newBoard[item[0]][item[1]] = 1;
+    }
+    setPlaying(false)
+    setBoard(newBoard);
 
-
-  function handleResize(rows,cols) {
-    setRows(rows)
-    setCols(cols)
-    setBoard(fillBoard(rows,cols,true))
   }
 
+  function handleResize(rows, cols) {
+    setRows(rows);
+    setCols(cols);
+    setBoard(fillBoard(rows, cols, true));
+  }
 
   useEffect(() => {
     interval = setInterval(() => {
       if (playing) {
-        setBoard(board => stepOneGeneration(board))
+        setBoard((board) => stepOneGeneration(board));
       }
-    },(101-(speed))*3)
+    }, (101 - speed) * 3);
 
-    return () => clearInterval(interval)
-  },[playing,speed])
-
-
-  
-
+    return () => clearInterval(interval);
+  }, [playing, speed]);
 
   return (
-    <div className="App">
+    <div className="App align-middle">
+      <div className="mt-[30%] md:mt-0 flex justify-center items-center">
+        <button
+          onClick={() => {
+            setBoard(fillBoard(rows, cols, true));
+            setPlaying(false);
+          }}
+        >
+          Randomise
+        </button>
+        <button
+          onClick={() => {
+            setBoard(fillBoard(rows, cols, false));
+            setPlaying(false);
+          }}
+        >
+          Clear
+        </button>
 
-      <div className="flex justify-center items-center m-2">
-        <button onClick={() => {setBoard(fillBoard(rows,cols,true)); setPlaying(false)}}>Randomise</button>
-        <button onClick={() => {setBoard(fillBoard(rows,cols,false)); setPlaying(false)}}>Clear</button>
-        <button onClick={() => setBoard(stepOneGeneration(board))}>step</button>
-        <button onClick={() => setPlaying(!playing)}><img src={playing ? pauseButton: playButton} width={12}/></button>
-    
-        <select onInput={event => setLayout(JSON.parse(event.target.value))}>
-          {configurations.map(configuration => {
+        <select onInput={(event) => setLayout(JSON.parse(event.target.value))}>
+          {configurations.map((configuration) => {
             return (
-              <option value={JSON.stringify(configuration.layout)}>{configuration.name}</option>
-            )
+              <option value={JSON.stringify(configuration.layout)}>
+                {configuration.name}
+              </option>
+            );
           })}
         </select>
+
+        <button onClick={() => setPlaying(!playing)}>
+          <img src={playing ? pauseButton : playButton} width={12} />
+        </button>
       </div>
 
-      <div className="flex justify-center">
-        <Board id="board" onCopy={() => saveLayout(board)} board={board} handleBoxClick={handleBoxClick}/>      
+      <div className="flex justify-center mt-3 md:mt-0">
+        <Board
+          id="board"
+          onCopy={() => saveLayout(board)}
+          board={board}
+          handleBoxClick={handleBoxClick}
+        />
       </div>
 
       <div className="flex justify-center mt-[20px]">
@@ -120,17 +138,38 @@ export default function App() {
       </div>
 
       <div className="flex justify-center">
-        <div className="flex space-x-4 p-2 bg-gray-100 border-gray-200 border-[3px] rounded-full">
-          <input type="range" min={window.innerWidth <= 600 ? 55 : 30} max={80} step={5} onInput={event => handleResize(event.target.value,cols)} value={rows} onChange={event => handleResize(event.target.value,cols)}/>
-          
-          <input type="range" min={30} max={80} step={5} onInput={event => handleResize(rows,event.target.value)} value={cols} onChange={event => handleResize(rows,event.target.value)}/>
-          
-          <input type="range" min={0} max={100} step={10} onInput={event => setSpeed(event.target.value)} value={speed} onChange={event => setSpeed(event.target.value)}/>
+        <div className="flex space-x-[28px] p-2 bg-gray-100 border-gray-200 border-[3px] rounded-full">
+          <input
+            type="range"
+            min={window.innerWidth <= 600 ? 55 : 30}
+            max={200}
+            step={5}
+            onInput={(event) => handleResize(event.target.value, cols)}
+            value={rows}
+            onChange={(event) => handleResize(event.target.value, cols)}
+          />
+
+          <input
+            type="range"
+            min={30}
+            max={200}
+            step={5}
+            onInput={(event) => handleResize(rows, event.target.value)}
+            value={cols}
+            onChange={(event) => handleResize(rows, event.target.value)}
+          />
+
+          <input
+            type="range"
+            min={0}
+            max={100}
+            step={10}
+            onInput={(event) => setSpeed(event.target.value)}
+            value={speed}
+            onChange={(event) => setSpeed(event.target.value)}
+          />
         </div>
       </div>
-      
     </div>
-    
-
-  )
+  );
 }
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,7 @@
 import { useRef, useEffect } from "react"
 
 export default function Board({ board, handleBoxClick, onCopy }) {
+    
     let canvas
 
     let mini = (window.innerWidth <= 600)
@@ -21,7 +22,7 @@ export default function Board({ board, handleBoxClick, onCopy }) {
         for (let i = 0; i < board.length; i++) {
             for (let j = 0; j < board[0].length; j++) {
                 context.fillStyle = board[i][j] ? "rgb(239 68 68)" : "white"
-                context.fillRect((j) * divider, (i) * divider, divider * .96, divider * .96)
+                context.fillRect((j) * divider, (i) * divider, divider * 0.96, divider * 0.96)
             }
         }
     })
@@ -29,24 +30,25 @@ export default function Board({ board, handleBoxClick, onCopy }) {
     return (
         <canvas 
             onClick={event => {
-            var rect = canvas.getBoundingClientRect();
+            if (!mini) {
+                var rect = canvas.getBoundingClientRect();
 
-            var x = Math.round((event.clientX - rect.left) / (divider))-1;
-            if (x >= board[0].length) {
-                x = board[0].length - 1
+                var x = Math.round((event.clientX - rect.left) / (divider))-1;
+                if (x >= board[0].length) {
+                    x = board[0].length - 1
+                }
+
+                var y = Math.round((event.clientY - rect.top) / (divider))-1;
+                if (y >= board.length) {
+                    y = board.length - 1
+                }
+
+                handleBoxClick(y,x)
+            } //just making the user not able to click squares on the board while on a smaller device is a temporary solution utnil I find out how to properly do it while using the zoom css function
+            else {
+                console.log("device too smol")
             }
-
-            var y = Math.round((event.clientY - rect.top) / (divider))-1;
-            if (y >= board.length) {
-                y = board.length - 1
-            }
-
-            if (mini) {
-                x *= 2
-                y *= 2
-            }
-
-            handleBoxClick(y, x)
+            
         }}  className="border-red-200 border-[5px] rounded-lg" 
             width={(board[0].length * divider)}
             height={(board.length * divider)}
